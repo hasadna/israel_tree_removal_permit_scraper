1a941d64583344e4c1d100f8053000f6afb2fbbb
Udi Oron <udioron@gmail.com>
2021-02-07 15:37:44 +0200

    Who needs backups?

--- fix_xlsx.py
+++ fix_xlsx.py
@@ -1,27 +1,30 @@
-import shutil
-from collections import Counter
+import logging
 from pathlib import Path
 
 from openpyxl import load_workbook
 
-target = Path(__file__).parent / "downloads"
-backup = Path(__file__).parent / "backup"
-backup.mkdir(exist_ok=True)
+logger = logging.getLogger(__name__)
 
-c = Counter()
-for p in target.glob("*.xlsx"):
-    print(p)
-    wb = load_workbook(p)
-    ws = wb.active
-    first = True
-    empty = []
-    for i, row in enumerate(ws.rows, 1):
-        if not any(c.value for c in row):
-            empty.append(i)
-    if empty:
-        print(empty)
-        shutil.copy(p, backup / p.name)
-        for idx in empty[::-1]:
-            print(idx)
-            ws.delete_rows(idx, 1)
-        wb.save(p)
+
+def fix_xlsx_files(folder: Path):
+    for p in folder.glob("*.xlsx"):
+        logger.info(f"Checking {p.name}...")
+        wb = load_workbook(p)
+        ws = wb.active
+        empty = []
+        for i, row in enumerate(ws.rows, 1):
+            if not any(c.value for c in row):
+                empty.append(i)
+        if empty:
+            logger.warning(
+                f"Fixing {p.name}: deleting empty rows #{','.join(str(x) for x in empty)}"
+            )
+            for idx in empty[::-1]:
+                print(idx)
+                ws.delete_rows(idx, 1)
+            wb.save(p)
+
+
+if __name__ == "__main__":
+    target = Path(__file__).parent / "downloads"
+    fix_xlsx_files(target)
