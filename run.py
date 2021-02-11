c820e535f36dfc493a9ff5a9a95422d04f0c43d7
Udi Oron <udioron@gmail.com>
2021-02-11 13:46:48 +0200

    fix: missing parameter in run.py

--- run.py
+++ run.py
@@ -3,19 +3,24 @@ from pathlib import Path
 
 from doc_parser import process_docs
 from retrieve_docs import retrieve_docs
-from settings import DOWNLOAD_FOLDER, CSV_EXPORTS_FOLDER
+from settings import DOWNLOAD_FOLDER, CSV_EXPORTS_FOLDER, TEXT_EXPORTS_FOLDER
 
 logger = logging.getLogger(__name__)
 
 
-def download_and_process_docs(downloads: Path, exports: Path):
+def download_and_process_docs(
+    downloads: Path,
+    exports: Path,
+    texts: Path,
+):
     logger.info("Starting...")
 
     downloads.mkdir(exist_ok=True)
     exports.mkdir(exist_ok=True)
+    texts.mkdir(exist_ok=True)
 
     retrieve_docs(downloads)
-    process_docs(downloads, exports)
+    process_docs(downloads, exports, texts)
     logger.info("Done!")
 
 
@@ -24,4 +29,8 @@ if __name__ == "__main__":
         format="[%(levelname)s %(asctime)s %(module)s:%(lineno)d] %(message)s",
         level=logging.INFO,
     )
-    download_and_process_docs(DOWNLOAD_FOLDER, CSV_EXPORTS_FOLDER)
+    download_and_process_docs(
+        DOWNLOAD_FOLDER,
+        CSV_EXPORTS_FOLDER,
+        TEXT_EXPORTS_FOLDER,
+    )
